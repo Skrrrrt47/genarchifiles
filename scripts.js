27ebfc79753d97996c10deb9ecbef84c1b8f9513
Thomas Eychenne <thomas.eychenne@epita.fr>
2024-11-24 21:06:45 +0100

    aaa

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Configuration PostgreSQL
 const pool = new Pool({
